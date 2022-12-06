0685893652fca506d8e13a34238ed836716369d9
MichaelNewberg <michael390@revature.net>
2022-12-06 11:22:30 -0600

    Primitives and Comments

---
+++ Java_Notes/DataTypes.java
@@ -0,0 +1,18 @@
+public class DataTypes {
+    /*Java represents the most basic of data in structures called primitives: these data types are the simplest
+    ones you can work with in Java, and we will be making extensive use of them while we look at other aspects of Java*/
+
+    //these types represent whole numbers
+    byte b=0; //8 bits in size
+    short s=0; //16 bits
+    int i=0;  //32 bits max size 2^32-1 
+    long l=0; //64 bits
+    //these types represent rational numbers (ie those with decimals)
+    float f=0f; //32 bits
+    double d=0.0;//64 bits
+    //this represents the unicode character
+    char c='C'; //16 bits
+    //this represents logical true/false
+    boolean bb=true;//1 bit
+
+}
