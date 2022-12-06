6f2b5976f185381428aa493f9c569f8c6a4db2a7
MichaelNewberg <michael390@revature.net>
2022-12-06 14:52:02 -0600

    do-while loop added

--- Java_Notes/Loops.java
+++ Java_Notes/Loops.java
@@ -2,9 +2,9 @@ public class Loops {
     public static void main(String[] args) {
         /*for(int i=0; i<=10; i++){
              System.out.println(i);
-        }
+        }*/
         int x=2;
-        while(x>0){
+       /*while(x>0){
             System.out.println(x);
             x--;
         }*/
@@ -16,15 +16,18 @@ public class Loops {
 
         //storing numbers and looping through an array
         int[] numbers={1,2,3,4,5};//index for this would be {0,1,2,3,4}
-        for(int x=0; x<numbers.length; x++){//.length references length of the array (5 in this case)
+        /*for(int x=0; x<numbers.length; x++){//.length references length of the array (5 in this case)
             //System.out.println(numbers[x]);
             if(numbers[x] % 2==0){
                 System.out.println("X is even this iteration for value: "+numbers[x]);
             }else{
                 System.out.println("X is odd this iteration for value:  "+numbers[x]);
             }
-        }
-
+        }*/
+        do {//do-while runs at least once before checking
+            System.out.println(x);
+            x--;
+        } while (x>=0);
         
     }
 }
