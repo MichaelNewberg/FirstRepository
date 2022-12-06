3b8ec0f12d8fa56e6df3616ec7aee6b7e0cdbdfe
MichaelNewberg <michael390@revature.net>
2022-12-06 15:09:11 -0600

    Operators notes

---
+++ Java_Notes/Operators.java
@@ -0,0 +1,24 @@
+public class Operators {
+    public static void main(String[] args) {
+        // (+) addition
+        // (-) subtraction
+        // (*) multiplication
+        // (/) division
+        // (%) modulus/remainder
+        // (=) assignment
+
+        //QoL operators
+        // (+=) addition of the variable to itself     x+=5;  is the same as x=x+5;
+        // (-=) subtraction of the variable to itself  x-=5;
+        // (++) increment operator adds 1              x++  OR  ++x
+        // (--) decrement operator subtracts 1         x--  OR  --x
+
+        //Logical operators
+        // (<, >) Less than/Greater than
+        // (==) equivalence
+        // (===) strict equivalence
+        // (!) not operator (inverses requirement)
+        // (&&) logical AND
+        // (||) logical OR
+    }
+}
