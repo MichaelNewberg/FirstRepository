91f1489689b97a272b7a9da4758b4d22a7786ab4
MichaelNewberg <michael390@revature.net>
2022-12-27 11:29:36 -0600

    docker build/push script

---
+++ Java_Notes/boot/create.sh
@@ -0,0 +1,5 @@
+# this bash script will be used to create my sports docker image
+# I will use this script instead of manually entering commands so i dont actually make a spelling mistake
+
+docker build -t michaelnewberg/practice:rolling .
+docker push michaelnewberg/practice:rolling
